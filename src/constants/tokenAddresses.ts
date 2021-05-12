87ed208bc455af3226a625ebd4a0cfb984af88c5
Mark Wagner <mark@quicsystems.com>
2021-05-11 20:47:49 -0500

    New rinkeby

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -8,9 +8,9 @@
 
 
 
-export const quic = '0x6139a60CbB4fbC729060CC44E193C489FE27246a'
-export const quicv2 = '0x6139a60CbB4fbC729060CC44E193C489FE27246a'
-export const quicAddress = '0x6139a60CbB4fbC729060CC44E193C489FE27246a'
-export const masterChefAddress = '0xcEe47f5f03D3975Ac51A80792Db7fE834D269Bf6'
+export const quic = '0xCE67DA1f65f62c0a2AF0C97CE396bE82F3a4dDCa'
+export const quicv2 = '0xCE67DA1f65f62c0a2AF0C97CE396bE82F3a4dDCa'
+export const quicAddress = '0xCE67DA1f65f62c0a2AF0C97CE396bE82F3a4dDCa'
+export const masterChefAddress = '0xcEEf5C19F30f6639d4CeBe6285eEd186dE15149F'
 export const wethPriceAddress = '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e'  //rinkeby
 export const quicPriceAddress = '0x2F31cabcFc3687044261B3F15b0e6F6b45504795'  // rinkeby
